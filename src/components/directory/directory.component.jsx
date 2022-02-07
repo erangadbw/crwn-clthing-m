63296b755d8ea84240a2c3db2f443c1f7e2813dd
eranga <erangadbw@gmail.com>
2022-02-07 15:55:34 +1100

    update our menu-items to navigate withRouther HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,9 +47,11 @@ class Directory extends React.Component {
 
     render(){
         return (
+            //...othersection props just maps the values in the props to a value of the same name
             <div className ='directory-menu'>
-                {this.state.sections.map(({title,imageUrl,id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl = {imageUrl} size = {size}/>
+                
+                {this.state.sections.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key={id} {...otherSectionProps}/>
                     ))}
                 
                 
