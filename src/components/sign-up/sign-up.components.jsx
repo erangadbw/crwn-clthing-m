d0d00c04cab8dcf31f8697915d2fd777b2f0b1df
eranga <erangadbw@gmail.com>
2022-02-08 16:00:49 +1100

    finished sign in and sign up componenets

--- src/components/sign-up/sign-up.components.jsx
+++ src/components/sign-up/sign-up.components.jsx
@@ -80,7 +80,7 @@ class SignUp extends React.Component {
                     <FormInput
                         type = 'password'
                         name = 'password'
-                        value = {password}
+                        value = {this.state.password}
                         onChange= {this.handleChange}
                         label = 'Password'
                         required
