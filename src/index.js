388a3c80e9250757330db9eaf112264633255354
eranga <erangadbw@gmail.com>
2022-02-17 12:34:24 +1100

    moved our directory and shop data into its own reducer, and created selectors

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from  'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-import store from './redux/store';
+import {store,persistor} from './redux/store';
+
 
 ReactDOM.render(
   
     <Provider store = {store}>
         <BrowserRouter>
+        <PersistGate persistor={persistor}>
          <App />
+         </PersistGate>
         </BrowserRouter>,
     </Provider>,
    document.getElementById('root')
